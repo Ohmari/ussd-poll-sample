e7d30ef6ac0ae0d0828257ecdfa089d07daf406c
Boahen Fred <boahendeycarter@gmail.com>
2018-06-17 07:14:07 -0700

    modified API to catch responses from user

--- src/main/java/com/boaheninc/ussd_app/web/IndexController.java
+++ src/main/java/com/boaheninc/ussd_app/web/IndexController.java
@@ -1,16 +1,17 @@
 package com.boaheninc.ussd_app.web;
 
+import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 import com.boaheninc.ussd_app.model.UssdParameters;
 
 @RestController
 @RequestMapping("/api")
 public class IndexController {
     
-	@PostMapping("/ussd/random-poll")
+	@PostMapping(value="/ussd/random-poll", consumes = {"application/x-www-form-urlencoded"})
     public String index(UssdParameters params) {
     	String response = "", sessionId, text, phoneNumber, serviceCode;
     	String[] textArray;
@@ -21,38 +22,27 @@ public class IndexController {
     	phoneNumber = params.getPhoneNumber();
     	serviceCode = params.getServiceCode();
     	
-    	//extract individual strings from text that are concatenated by *
+    	/*
+    	 * extract individual strings from text that are concatenated by *
+    	 * Assumptions are neither of these parameters will be null during the 
+    	 * initiation of the USSD call hence no checks for null in the code
+    	 */
+    	
     	textArray = text.split("\\*");
-    	   	
-    	System.out.println("text " + text);
+    	int separatorOccurance = StringUtils.countOccurrencesOf(text, "*");
         if(text == "") {
     		response = "CON Welcome to the DISSOLVE GFA Polls\nPress 1 to Continue to Polls"
     				+ "\nPress 0 to exit Polls";
-        }else if(Integer.parseInt(text)==1) {
-    		//check user's response and CONTINUE or TERMINATE session
-    		switch(Integer.parseInt(textArray[0])) {
-      	  case 1:
+        }else if(text.contentEquals("1")) {
       		  response = "CON Please provide your name\n"; 
-      		  break;
-      	default:
+        }else if(text.contentEquals("0")) {
       		  response = "END";
-      		  break;
-      	   }
-        }else if(!textArray[1].isEmpty() && textArray[1]!="" && textArray.length==2){
+        }else if(separatorOccurance==1){
         	response = "CON Press 1 to vote for the motion"
     				+ "\nPress 2 to vote against the motion"; 
-        }else if(!textArray[2].isEmpty() && textArray[2]!="" && textArray.length==3) {
-        	//check user's response and CONTINUE or TERMINATE session
-        	switch(Integer.parseInt(textArray[2])) {
-      	  case 1:
-      	  case 2:
-      		  response = "CON give a reason for your chosen option\n"; 
-      		  break;
-      	  default:
-      		  response = "END";
-      		  break;
-      	    }
-        }else if(!textArray[2].isEmpty() && textArray[2]!="" && textArray.length==4) {
+        }else if(separatorOccurance==2) {
+        	response = "CON give a reason for your chosen option\n"; 
+        }else if(separatorOccurance==3) {
         	//save data to db [currently only saves into an array]
         	data = new String[textArray.length];
         	data[0] = phoneNumber;
